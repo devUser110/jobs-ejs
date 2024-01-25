b209542f9b6a8e01cacb39a3a22d5c56a9ded079
devUser110 <jr.devon@gmail.com>
2024-01-24 21:23:02 -0500

    fixed validation issues

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -6,10 +6,12 @@ const registerShow = (req, res) => {
 };
 
 const registerDo = async (req, res, next) => {
+  let validation_errors = false;
   if (req.body.password != req.body.password1) {
     req.flash("error", "The passwords entered do not match.");
-    res.render("register", { errors: req.flash.errors });
+    validation_errors = true;
   }
+
   try {
     await User.create(req.body);
   } catch (e) {
@@ -20,9 +22,17 @@ const registerDo = async (req, res, next) => {
     } else {
       return next(e);
     }
-    return res.render("register");
+    validation_errors = true;
   }
-  res.redirect("/");
+
+  // if there are no validation errors then redirect to main paige
+  // else pass errors to be rendered in the register view
+
+  if (!validation_errors) {
+    res.redirect("/");
+  } else {
+    return res.render("register", { errors: req.flash("error") });
+  }
 };
 
 const logoff = (req, res) => {
